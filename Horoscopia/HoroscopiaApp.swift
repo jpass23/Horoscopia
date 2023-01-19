1ffe7c04e84b59813d42462a00d5b3ec121f3820
jpass23 <jpass23@live.unc.edu>
2023-01-18 23:24:23 -0500

    Initial Commit

---
+++ Horoscopia/HoroscopiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoroscopiaApp.swift
+//  Horoscopia
+//
+//  Created by Jaden Passero on 1/18/23.
+//
+
+import SwiftUI
+
+@main
+struct HoroscopiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
